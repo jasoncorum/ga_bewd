429e11998f0239be01fba0197dd7e57159099d0a
Jason Corum <jason.corum@gmail.com>
2014-05-14 21:45:03 -0400

    Revert "Complete ext_teddit"
    This reverts commit 54a2ed93da1f8ed173f64d74ede7b678e468161b.
    
    Reverting this commit since I saved update the to ex_teddit_conditional in the wrong directory. Update should have been committed to the Student directory instead of the Classes directory.

--- classes/02_Variables_Conditionals/starter_code/ex_teddit_conditional.rb
+++ classes/02_Variables_Conditionals/starter_code/ex_teddit_conditional.rb
@@ -6,7 +6,6 @@
 
 def get_input
   #Get input from the user.
-  gets.chomp
 end
 
 def calculate_upvotes(story, category)
@@ -14,16 +13,6 @@ def calculate_upvotes(story, category)
 		# If the Story is about cats multiply the upvotes by 5
 		# If the Story is about bacon multiply the upvotes by 8
 		# If the Story is about Food it gets 3 times the upvotes.
-		upvotes = 1
-
-		if category.include?("cat") 
-			upvotes * 5
-		elsif category.include?("bacon")
-			upvotes * 8
-		else category.include?("food")
-			upvotes * 3
-	end
-			
 
 	#For example:
 	# "Cats frolic despite tuna shortage" should give you 5 times the upvotes!

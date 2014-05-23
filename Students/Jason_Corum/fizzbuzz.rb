a134203384cbc2979b168cc32b20d02b4377488b
Jason Corum <jason.corum@gmail.com>
2014-05-23 00:05:48 -0400

    Figuring out ternary operators on fizzbuzz.rb

--- Students/Jason_Corum/fizzbuzz.rb
+++ Students/Jason_Corum/fizzbuzz.rb
@@ -8,25 +8,28 @@
 # if something prints false that means its a failing case
  
 def fizzbuzz(number)
-  response = ''
-  response += 'fizz' if number % 3 == 0
-  response += 'buzz' if number % 5 == 0
-  return response if !response.empty?
-  number
+
+	(number % 3 == 0) && (number % 5 == 0) ? "fizzbuzz"
+	: (number % 3) == 0 ? "fizz"
+	: (number % 5) == 0 ? "buzz"
+	: number
+
 end
+
+# hash[:category].include?("cats") ? hash[:category].include?("bacon") ? hash[:upvotes] * 40 : hash[:upvotes] * 8 : hash[:category].include?("food") ? hash[:upvotes] * 3 : hash[:upvotes] * 1
  
-# 1.upto(100) do |num|
-#   puts fizzbuzz(num)
-# end
+1.upto(100) do |num|
+  puts fizzbuzz(num)
+end
  
  
 def assert(truthy)
   raise "There was an error" if !truthy
 end
  
-assert fizzbuzz(1) == 1
-assert fizzbuzz(2) == 2
-assert fizzbuzz(3) == 'fizz'
+# assert fizzbuzz(1) == 1
+# assert fizzbuzz(2) == 2
+# assert fizzbuzz(3) == 'fizz'
 # assert fizzbuzz(4) == 4
 # assert fizzbuzz(5) == 'buzz'
 # assert fizzbuzz(9) == 'fizz'

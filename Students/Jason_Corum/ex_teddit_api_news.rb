58d153dced4a1bebdc841a6e510e22ffa5038918
Jason Corum <jason.corum@gmail.com>
2014-05-25 23:37:39 -0400

    Complete ex_teddit_api_news and working with NYC's bikeshare API

--- Students/Jason_Corum/ex_teddit_api_news.rb
+++ Students/Jason_Corum/ex_teddit_api_news.rb
@@ -12,22 +12,52 @@ require 'json'
 
 stories = []
 
-response = JSON.parse(RestClient.get 'http://www.reddit.com/.json')
+###########################################################################
 
-response['data']['children'].each do |story|
+# The code below is for accessing http://mashable.com/stories.json
+
+response = JSON.parse(RestClient.get 'http://mashable.com/stories.json')
+
+response['new'].each do |story|
 	hash = {}
 
-	hash[:title] = story['data']['title']
-	hash[:categories] = story['data']['subreddit']
+	hash[:title] = story['title']
+	hash[:categories] = story['channel']
 
-	hash[:upvotes] = story['data']['ups']
+	hash[:upvotes] = story['shares']['total']
 
-	hash[:categories].include?("cats") ? hash[:upvotes] *= 5
-	: hash[:categories].include?("bacon") ? hash[:upvotes] *= 8
-	: hash[:categories].include?("food") ? hash[:upvotes] *= 3
+	hash[:categories].include?("cat".capitalize) ? hash[:upvotes] *= 5
+	: hash[:categories].include?("bacon".capitalize) ? hash[:upvotes] *= 8
+	: hash[:categories].include?("food".capitalize) ? hash[:upvotes] *= 3
 	: hash[:upvotes]
 
-# The commented out code below would handle instances where where multiple categories would be included (ie. a single story with "cats," "bacon," and "food" as categories.
+	stories<<hash
+end
+
+stories.each do |story|
+	puts "Story: #{story[:title]}, Category: #{story[:categories]}, Upvotes: #{story[:upvotes]}"
+end
+
+###########################################################################
+
+# The commented out code below is for accessing http://www.reddit.com/.json
+
+# response = JSON.parse(RestClient.get 'http://www.reddit.com/.json')
+
+# response['data']['children'].each do |story|
+# 	hash = {}
+
+# 	hash[:title] = story['data']['title']
+# 	hash[:categories] = story['data']['subreddit']
+
+# 	hash[:upvotes] = story['data']['ups']
+
+# 	hash[:categories].include?("cats") ? hash[:upvotes] *= 5
+# 	: hash[:categories].include?("bacon") ? hash[:upvotes] *= 8
+# 	: hash[:categories].include?("food") ? hash[:upvotes] *= 3
+# 	: hash[:upvotes]
+
+	# The commented out code below would handle instances where where multiple categories would be included (ie. a single story with "cats," "bacon," and "food" as categories.
 
 	# hash[:categories].include?("cats") && hash[:categories].include("bacon") && hash[:categories].include("food") ? hash[:upvotes] *= 120 
 	# : hash[:categories].include?("cats") && hash[:categories].include("bacon") ? hash[:upvotes] *= 40
@@ -38,9 +68,9 @@ response['data']['children'].each do |story|
 	# : hash[:categories].include?("food") ? hash[:upvotes] *= 3
 	# : hash[:upvotes]
 
-	stories<<hash
-end
+# 	stories<<hash
+# end
 
-stories.each do |story|
-	puts "Story: #{story[:title]}, Category: #{story[:categories]}, Upvotes: #{story[:upvotes]}"
-end
+# stories.each do |story|
+# 	puts "Story: #{story[:title]}, Category: #{story[:categories]}, Upvotes: #{story[:upvotes]}"
+# end
